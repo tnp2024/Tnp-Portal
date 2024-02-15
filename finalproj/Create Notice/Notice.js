b93b32f50801ab1455254c1710f53d5fdbb0c198
tnp2024 <160101955+tnp2024@users.noreply.github.com>
2024-02-15 11:51:46 +0530

    Add files via upload

---
+++ finalproj/Create Notice/Notice.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const cardBody = document.querySelector('.card-body');
+
+    dropdownBtn.addEventListener('click', function() {
+        if (cardBody.style.display === 'none' || cardBody.style.display === '') {
+            cardBody.style.display = 'block';
+        } else {
+            cardBody.style.display = 'none';
+        }
+    });
+});
